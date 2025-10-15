6a69625239291d0b2cc9f6aaa200c0ae084b841e
tehzarathustra <tehzarathustra@gmail.com>
2025-10-15 14:08:02 +0200

    Adds project-local configs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -23,31 +23,6 @@ vim.diagnostic.config({
     severity_sort = true,
 })
 
--- @TODO make it project-local (exrc)
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'javascript',
-    callback = function()
-        vim.opt_local.makeprg = 'node --test-reporter=tap %'
-        vim.opt_local.errorformat ='  location: "%f:%l:%c"'
-    end,
-})
+-- allow project local configs
+vim.opt.exrc = true
 
--- @TODO, start doing it in another file
-local root = vim.fn.expand('~/arcadia/distribution_interface/frontend/static')
-
-local function setup_makeprg()
-  local dir = vim.fn.expand('%:p:h')
-  local rel = dir:gsub(vim.pesc(root .. '/'), '')
-  vim.opt_local.makeprg = string.format(
-    [[sh -lc "cd %s && npm run tanker-sync %s"]],
-    root,
-    vim.fn.shellescape(rel)
-  )
-end
-
-vim.api.nvim_create_augroup('KeysetsSync', {clear = true})
-vim.api.nvim_create_autocmd('BufEnter', {
-  pattern = root .. '/**/keysets.js',
-  callback = setup_makeprg,
-  group = 'KeysetsSync',
-})
